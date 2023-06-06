ef3562e3d9c82ebdcb7af0f41b4c8b09fb9a12f4
Jerome Haltom <jhaltom@alethic.solutions>
2023-06-06 10:11:02 -0500

    Not really working, but a start to see.#

---
+++ src/IKVM.Extensions.Logging.Java.Util.Tests/LoggerHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKVM.Extensions.Logging.Java.Util.Tests
+{
+
+    public class LoggerHandler
+    {
+
+
+
+    }
+
+}
---
+++ src/IKVM.Extensions.Logging.Java.Util/LoggerHandler.cs
@@ -0,0 +1,39 @@
+﻿using java.util.logging;
+
+using Microsoft.Extensions.Logging;
+
+namespace IKVM.Extensions.Logging.Java.Util
+{
+
+    public class LoggerHandler : Handler
+    {
+
+        readonly ILoggerFactory factory;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="factory"></param>
+        public LoggerHandler(ILoggerFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public override void publish(LogRecord record)
+        {
+
+        }
+
+        public override void close()
+        {
+
+        }
+
+        public override void flush()
+        {
+
+        }
+
+    }
+
+}
